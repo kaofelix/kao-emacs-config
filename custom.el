e40f191ce52c6c2e510f27f6b507994b2044e082
Kao Félix <kcfelix@gmail.com>
2014-09-22 23:13:34 -0300

    Auto-revert mode.

--- custom.el
+++ custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-revert-check-vc-info t)
+ '(global-auto-revert-mode t)
  '(inhibit-startup-screen t)
  '(smartparens-global-mode t))
 (custom-set-faces
