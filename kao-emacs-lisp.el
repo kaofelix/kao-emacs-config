46a97171714f3517b3d834e07672e1acb4d4b1d7
Kao Félix <kcfelix@gmail.com>
2014-09-23 11:35:17 -0300

    slime-nav for elisp.

---
+++ kao-emacs-lisp.el
@@ -0,0 +1,17 @@
+;;; kao-emacs-lisp.el --- Config for Emacs Lisp editing
+
+;; Copyright (C) 2014  Kao Felix
+
+;; Author: Kao Felix <kaofelix@Steelix.local>
+;; Keywords: local, lisp
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(add-hook 'emacs-lisp-mode-hook 'elisp-slime-nav-mode)
+
+(provide 'kao-emacs-lisp)
+;;; kao-emacs-lisp.el ends here
