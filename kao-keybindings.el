73779677827759b501e6bd58146dd5e853296ea3
Kao Félix <kcfelix@gmail.com>
2014-09-23 12:27:59 -0300

    Regexp search by default.

--- kao-keybindings.el
+++ kao-keybindings.el
@@ -53,5 +53,11 @@
 (require 'cua-base)
 (global-set-key (kbd "C-SPC") 'cua-set-mark)
 
+;; Use regex searches by default.
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "\C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
+
 (provide 'kao-keybindings)
 ;;; kao-keybindings.el ends here
