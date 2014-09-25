acc3ab773a28da7865afbef68f2836bf1ee7aa16
Kao Félix <kcfelix@gmail.com>
2014-09-25 18:20:09 -0300

    Functions for renaming and deleting current buffer file.

--- kao-defuns.el
+++ kao-defuns.el
@@ -57,6 +57,35 @@ already inside a project."
          (clear-this-command-keys t)
          (setq unread-command-events (list last-input-event))))))
 
+(defun rename-current-buffer-file ()
+  "Rename current buffer and file it is visiting."
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " filename)))
+        (if (get-buffer new-name)
+            (error "A buffer named '%s' already exists!" new-name)
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil)
+          (message "File '%s' successfully renamed to '%s'"
+                   name (file-name-nondirectory new-name)))))))
+
+(defun delete-current-buffer-file ()
+  "Remove file connected to current buffer and kill buffer."
+  (interactive)
+  (let ((filename (buffer-file-name))
+        (buffer (current-buffer))
+        (name (buffer-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (ido-kill-buffer)
+      (when (yes-or-no-p "Are you sure you want to remove this file? ")
+        (delete-file filename)
+        (kill-buffer buffer)
+        (message "File '%s' successfully removed" filename)))))
 
 (provide 'kao-defuns)
 ;;; kao-defuns.el ends here
