cd67fdadaef6e002aaa97b63c1663b61e611b87d
Kao Félix <kcfelix@gmail.com>
2014-10-06 15:57:10 -0300

    auto-fill mode for markdown.

---
+++ setup-markdown.el
@@ -0,0 +1,16 @@
+;;; setup-markdown.el --- Setup markdown
+
+;; Copyright (C) 2014  Kao Felix
+
+;; Author: Kao Felix <kcfelix@gmail.com>
+;; Keywords: local, wp
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+(add-hook 'markdown-mode-hook 'auto-fill-mode)
+
+(provide 'setup-markdown)
+;;; setup-markdown.el ends here
