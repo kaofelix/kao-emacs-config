799e28dd327c008c4688c804a2ea0350cbedb03f
Kao Félix <kcfelix@gmail.com>
2014-10-22 13:31:58 -0300

    New indentation for custom.el, apparently done automatically by Emacs 24.4.

--- custom.el
+++ custom.el
@@ -5,14 +5,18 @@
  ;; If there is more than one, they won't work right.
  '(ahs-face-check-include-overlay t)
  '(ahs-idle-interval 0.5)
- '(ahs-inhibit-face-list (quote (font-lock-comment-delimiter-face font-lock-comment-face font-lock-doc-face font-lock-doc-string-face font-lock-string-face font-lock-keyword-face)))
+ '(ahs-inhibit-face-list
+   (quote
+    (font-lock-comment-delimiter-face font-lock-comment-face font-lock-doc-face font-lock-doc-string-face font-lock-string-face font-lock-keyword-face)))
  '(auto-revert-check-vc-info t)
  '(browse-kill-ring-display-duplicates nil)
  '(browse-kill-ring-highlight-current-entry t)
  '(browse-kill-ring-highlight-inserted-item (quote pulse))
  '(coffee-tab-width 2)
  '(company-idle-delay 0)
- '(custom-safe-themes (quote ("961c16955aad99a73dfb711b31d2ac5208cb4bc89e40d28563a91455826f4da1" default)))
+ '(custom-safe-themes
+   (quote
+    ("961c16955aad99a73dfb711b31d2ac5208cb4bc89e40d28563a91455826f4da1" default)))
  '(dired-dwim-target t)
  '(global-auto-revert-mode t)
  '(global-git-gutter-mode t)
@@ -29,12 +33,30 @@
  '(rspec-use-spring-when-possible nil)
  '(rspec-use-zeus-when-possible nil)
  '(smartparens-global-mode t)
- '(term-bind-key-alist (quote (("C-c C-c" . term-interrupt-subjob) ("C-c C-e" . term-send-esc) ("C-p" . previous-line) ("C-n" . next-line) ("C-s" . isearch-forward) ("C-r" . isearch-backward) ("C-m" . term-send-return) ("C-y" . term-paste) ("M-f" . term-send-forward-word) ("M-b" . term-send-backward-word) ("M-p" . term-send-up) ("M-n" . term-send-down) ("M-d" . term-send-forward-kill-word) ("M-DEL" . term-send-backward-kill-word) ("M-r" . term-send-reverse-search-history) ("M-," . term-send-raw) ("M-." . comint-dynamic-complete))))
+ '(term-bind-key-alist
+   (quote
+    (("C-c C-c" . term-interrupt-subjob)
+     ("C-c C-e" . term-send-esc)
+     ("C-p" . previous-line)
+     ("C-n" . next-line)
+     ("C-s" . isearch-forward)
+     ("C-r" . isearch-backward)
+     ("C-m" . term-send-return)
+     ("C-y" . term-paste)
+     ("M-f" . term-send-forward-word)
+     ("M-b" . term-send-backward-word)
+     ("M-p" . term-send-up)
+     ("M-n" . term-send-down)
+     ("M-d" . term-send-forward-kill-word)
+     ("M-DEL" . term-send-backward-kill-word)
+     ("M-r" . term-send-reverse-search-history)
+     ("M-," . term-send-raw)
+     ("M-." . comint-dynamic-complete))))
  '(term-unbind-key-list (quote ("C-z" "C-x" "C-c" "C-y" "<ESC>")))
  '(whitespace-action (quote (auto-cleanup)))
  '(whitespace-global-modes nil)
  '(whitespace-style (quote (face trailing tabs)))
- '(whitespace-trailing (quote whitespace-space-before-tab)))
+ '(whitespace-trailing (quote whitespace-space-before-tab) t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
