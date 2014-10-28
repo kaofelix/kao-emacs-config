461c1622b09de0d280b57a8070f7b341924e1033
Kao Félix <kcfelix@gmail.com>
2014-10-28 12:48:16 -0300

    Add function to toggle between solarized light/dark.

--- lisp/setup-theme.el
+++ lisp/setup-theme.el
@@ -10,5 +10,22 @@
 (setq solarized-use-less-bold t)
 (load-theme 'solarized-light t)
 
+;; Warning: currently, disabling and enabling themes many times crashes latest
+;; Mac OSX Homebrew Emacs version:
+;; "GNU Emacs 24.4.1 (x86_64-apple-darwin14.0.0, NS apple-appkit-1343.14) of 2014-10-24"
+(defun solarized-light-switch ()
+  "Toggle between solarized light and dark."
+  (interactive)
+  (lexical-let* ((current-solarized (cl-find-if
+                                     (lambda (sym) (string-prefix-p "solarized-" (symbol-name sym)))
+                                     custom-enabled-themes))
+                 (other-solarized (if (string-suffix-p "-dark" (symbol-name current-solarized))
+                                      'solarized-light
+                                    'solarized-dark)))
+    (disable-theme current-solarized)
+    (load-theme other-solarized)))
+
+(global-set-key (kbd "C-x C-t") 'solarized-light-switch)
+
 (provide 'setup-theme)
 ;;; setup-theme.el ends here
