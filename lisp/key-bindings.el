50214e466b93ac78f94d36b605ec72fde6aecd8b
Kao Félix <kcfelix@gmail.com>
2014-10-28 16:09:44 -0300

    Remove Fullscreen shortcut, default one (F11) is enough.

--- lisp/key-bindings.el
+++ lisp/key-bindings.el
@@ -16,9 +16,6 @@
 (setq mac-option-modifier 'meta)
 (setq mac-right-option-modifier nil)
 
-;; ^⌘F: Fullscreen on Mac
-(global-set-key (kbd "<C-s-268632070>") 'toggle-frame-fullscreen)
-
 ;; Magit
 (global-set-key (kbd "C-c g") 'magit-status-project-dwim)
 
