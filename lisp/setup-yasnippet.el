b8c12e947a3510140903b7158fb38fedff35d7f7
Kao Félix <kcfelix@gmail.com>
2014-10-29 10:29:07 -0300

    Some cleanup on yasnippet setup.

--- lisp/setup-yasnippet.el
+++ lisp/setup-yasnippet.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2014  Kao Felix
 
-;; Author: Kao Felix(require 'yasnippet) <kcfelix@gmail.com>
+;; Author: Kao Felix <kcfelix@gmail.com>
 ;; Keywords: local, convenience
 
 ;;; Commentary:
@@ -11,40 +11,38 @@
 
 ;;; Code:
 
-(eval-after-load 'yasnippet
-  '(progn
+(with-eval-after-load 'yasnippet
+  ;; Jump to end of snippet definition
+  (define-key yas-keymap (kbd "<return>") 'yas/exit-all-snippets)
 
-     ;; Jump to end of snippet definition
-     (define-key yas-keymap (kbd "<return>") 'yas/exit-all-snippets)
+  ;; Inter-field navigation
+  (defun yas/goto-end-of-active-field ()
+    (interactive)
+    (let* ((snippet (car (yas--snippets-at-point)))
+           (position (yas--field-end (yas--snippet-active-field snippet))))
+      (if (= (point) position)
+          (move-end-of-line 1)
+        (goto-char position))))
 
-     ;; Inter-field navigation
-     (defun yas/goto-end-of-active-field ()
-       (interactive)
-       (let* ((snippet (car (yas--snippets-at-point)))
-              (position (yas--field-end (yas--snippet-active-field snippet))))
-         (if (= (point) position)
-             (move-end-of-line 1)
-           (goto-char position))))
+  (defun yas/goto-start-of-active-field ()
+    (interactive)
+    (let* ((snippet (car (yas--snippets-at-point)))
+           (position (yas--field-start (yas--snippet-active-field snippet))))
+      (if (= (point) position)
+          (move-beginning-of-line 1)
+        (goto-char position))))
 
-     (defun yas/goto-start-of-active-field ()
-       (interactive)
-       (let* ((snippet (car (yas--snippets-at-point)))
-              (position (yas--field-start (yas--snippet-active-field snippet))))
-         (if (= (point) position)
-             (move-beginning-of-line 1)
-           (goto-char position))))
+  (define-key yas-keymap (kbd "C-e") 'yas/goto-end-of-active-field)
+  (define-key yas-keymap (kbd "C-a") 'yas/goto-start-of-active-field)
 
-     (define-key yas-keymap (kbd "C-e") 'yas/goto-end-of-active-field)
-     (define-key yas-keymap (kbd "C-a") 'yas/goto-start-of-active-field)
+  ;; No dropdowns please, yas
+  (setq yas-prompt-functions '(yas/ido-prompt yas/completing-prompt))
 
-     ;; No dropdowns please, yas
-     (setq yas-prompt-functions '(yas/ido-prompt yas/completing-prompt))
+  ;; No need to be so verbose
+  (setq yas-verbosity 1)
 
-     ;; No need to be so verbose
-     (setq yas-verbosity 1)
-
-     ;; Wrap around region
-     (setq yas-wrap-around-region t)))
+  ;; Wrap around region
+  (setq yas-wrap-around-region t))
 
 (provide 'setup-yasnippet)
 ;;; setup-yasnippet.el ends here
