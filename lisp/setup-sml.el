7dca0bf58faa17234357488eef012bb3ac443492
Kao Félix <kcfelix@gmail.com>
2014-10-29 03:24:02 -0300

    send-function binding for sml-mode.

--- lisp/setup-sml.el
+++ lisp/setup-sml.el
@@ -11,7 +11,8 @@
 
 ;;; Code:
 (with-eval-after-load 'sml-mode
-  (define-key sml-mode-map (kbd "M-SPC") nil))
+  (define-key sml-mode-map (kbd "M-SPC") nil)
+  (define-key sml-mode-map (kbd "C-c C-f") 'sml-send-function))
 
 (provide 'setup-sml)
 ;;; setup-sml.el ends here
