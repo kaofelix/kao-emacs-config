1cc41581b43501bbae09dd768a56433495ba6bb2
Kao Félix <kcfelix@gmail.com>
2014-10-31 15:22:03 -0300

    No more search field in customize.

--- custom.el
+++ custom.el
@@ -17,6 +17,7 @@
  '(custom-safe-themes
    (quote
     ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(custom-search-field nil)
  '(dired-dwim-target t)
  '(flycheck-indication-mode nil)
  '(global-auto-revert-mode t)
