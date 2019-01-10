54294d38d2ba84961cb846acc35e9a3d3c46c015
Kao Félix <kcfelix@gmail.com>
2019-01-10 23:47:29 +0100

    Refactor setup-theme and change light theme to hard.

--- lisp/setup-theme.el
+++ lisp/setup-theme.el
@@ -13,12 +13,15 @@
 (defvar kao/current-theme-file (expand-file-name "current-theme" cache-and-saves-dir))
 (defvar kao/current-theme 'gruvbox-dark-soft)
 
+(defvar kao/light-theme 'gruvbox-light-hard)
+(defvar kao/dark-theme 'gruvbox-dark-soft)
+
 (defun kao/theme-light-switch ()
-  "Toggle between light and dark theme (currently gruvbox)."
+  "Toggle between light and dark theme."
   (interactive)
-  (if (memq 'gruvbox-dark-soft custom-enabled-themes)
-      (kao/set-theme-and-save 'gruvbox-light-soft)
-    (kao/set-theme-and-save 'gruvbox-dark-soft)))
+  (if (memq kao/light-theme custom-enabled-themes)
+      (kao/set-theme-and-save kao/dark-theme)
+    (kao/set-theme-and-save kao/light-theme)))
 
 (defun kao/set-theme-and-save (new-theme)
   "Set the current theme to `NEW-THEME' and save it to a theme file."
@@ -27,9 +30,14 @@
   (setq kao/current-theme new-theme)
   (with-temp-file kao/current-theme-file
     (emacs-lisp-mode)
-    (recentf-dump-variable 'kao/current-theme)
+    (kao/dump-theme-variable 'kao/current-theme)
     (beginning-of-line)
     (delete-char -1)))
+
+(defun kao/dump-theme-variable (theme-variable)
+  "Insert \"(setq THEME-VARIABLE theme)\" in the current buffer."
+  (let ((value (symbol-value theme-variable)))
+    (insert (format "\n(setq %S '%S)\n" theme-variable value))))
 
 (load kao/current-theme-file 'noerror 'nomessage)
 (kao/set-theme-and-save kao/current-theme)
