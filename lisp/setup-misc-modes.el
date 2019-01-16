8ffc3377d6b3c9d400b996ed066da82654df2324
Kao Felix <kcfelix@gmail.com>
2019-01-16 17:51:10 +0100

    Key bindings for git-gutter

--- lisp/setup-misc-modes.el
+++ lisp/setup-misc-modes.el
@@ -112,7 +112,17 @@
 
 (diminish 'undo-tree-mode)
 (diminish 'smartparens-mode)
-(diminish 'git-gutter-mode)
+
+(use-package git-gutter
+  :diminish git-gutter-mode
+  :bind
+  ("C-x C-g" . git-gutter)
+  ("C-x v =" . git-gutter:popup-hunk)
+  ("C-x p" . git-gutter:previous-hunk)
+  ("C-x n" . git-gutter:next-hunk)
+  ("C-x v s" . git-gutter:stage-hunk)
+  ("C-x v r" . git-gutter:revert-hunk)
+  ("C-x v SPC" . git-gutter:mark-hunk))
 
 ;; Tramp with vc
 (setq vc-ignore-dir-regexp
