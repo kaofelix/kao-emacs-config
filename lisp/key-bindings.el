9d4d622b03e1381bec8ebd351901623289e77b83
Kao Felix <kcfelix@gmail.com>
2019-01-21 11:58:00 +0100

    (setq help-char nil) is breaking something, should investigate later

--- lisp/key-bindings.el
+++ lisp/key-bindings.el
@@ -27,7 +27,6 @@
 (global-set-key [remap just-one-space] 'cycle-spacing)
 
 ;; C-h for backspace
-(setq help-char nil)
 (global-unset-key [?\C-h])
 (define-key key-translation-map [?\C-h] [?\C-?])
 (define-key key-translation-map [?\M-\C-h] [?\M-\C-?])
