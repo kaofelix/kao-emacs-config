dfcfeb947f182edbd88b18482968c83c5e79e225
Kao Félix <kcfelix@gmail.com>
2019-02-27 21:24:07 +0100

    Revert "Use lsp-mode for Rust"
    This reverts commit 1625a24a6a4e1dcd5fa4d784916aed53b4b4c351.

--- custom.el
+++ custom.el
@@ -57,9 +57,6 @@
  '(imenu-auto-rescan t)
  '(inhibit-startup-screen t)
  '(locate-command "mdfind")
- '(lsp-prefer-flymake nil)
- '(lsp-ui-sideline-show-hover nil)
- '(lsp-ui-sideline-show-symbol nil)
  '(magit-diff-use-overlays nil)
  '(magit-save-repository-buffers (quote dontask))
  '(magit-status-headers-hook
@@ -92,7 +89,7 @@
  '(org-src-fontify-natively t)
  '(package-selected-packages
    (quote
-    (wgrep-helm nginx-mode rjsx-mode flycheck-rust company-lsp lsp-ui lsp-mode drag-stuff multi-term git-timemachine highlight-symbol delight comment-dwim-2 yasnippet-snippets yaml-mode which-key visual-regexp use-package undo-tree toml-mode smartparens smart-mode-line shackle rainbow-mode paren-face paradox pallet org-tree-slide org-bullets neotree multiple-cursors highlight-quoted highlight-numbers helm-swoop helm-projectile helm-descbinds helm-c-yasnippet helm-ag gruvbox-theme gitignore-mode git-gutter expand-region exec-path-from-shell elpy elisp-slime-nav el-patch dtrt-indent docker direnv company-terraform cargo avy)))
+    (wgrep-helm nginx-mode rjsx-mode flycheck-rust drag-stuff multi-term git-timemachine highlight-symbol delight comment-dwim-2 yasnippet-snippets yaml-mode which-key visual-regexp use-package undo-tree toml-mode smartparens smart-mode-line shackle rainbow-mode racer paren-face paradox pallet org-tree-slide org-bullets neotree multiple-cursors highlight-quoted highlight-numbers helm-swoop helm-projectile helm-descbinds helm-c-yasnippet helm-ag gruvbox-theme gitignore-mode git-gutter expand-region exec-path-from-shell elpy elisp-slime-nav el-patch dtrt-indent docker direnv company-terraform cargo avy)))
  '(paradox-column-width-package 25)
  '(paradox-column-width-version 13)
  '(paradox-execute-asynchronously t)
--- lisp/setup-prog-mode.el
+++ lisp/setup-prog-mode.el
@@ -66,14 +66,6 @@
    ("C-n" . #'company-select-next-or-abort)
    ("C-p" . #'company-select-previous-or-abort)))
 
-(use-package lsp-mode
-  :commands lsp
-  :config
-  (remove-hook 'lsp-eldoc-hook 'lsp-document-highlight))
-
-(use-package lsp-ui :commands lsp-ui-mode)
-(use-package company-lsp :commands company-lsp)
-
 (add-hook 'prog-mode-hook 'hl-line-mode)
 (add-hook 'prog-mode-hook 'prettify-symbols-mode)
 
