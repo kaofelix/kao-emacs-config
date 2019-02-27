ce1d8fbc0027d0574636089aa9ca2208db9a7da5
Kao Felix <kcfelix@gmail.com>
2019-02-27 15:34:06 +0100

    Add shortcut for vc-diff

--- lisp/setup-misc-modes.el
+++ lisp/setup-misc-modes.el
@@ -127,6 +127,7 @@ Switch to the project specific term buffer if it already exists."
   :bind
   ("C-x C-g" . #'git-gutter)
   ("C-x v =" . #'git-gutter:popup-hunk)
+  ("C-x v C-=" . #'vc-diff)
   ("C-x p" . #'git-gutter:previous-hunk)
   ("C-x n" . #'git-gutter:next-hunk)
   ("C-x v s" . #'git-gutter:stage-hunk)
