c0338ed7fa6c9180c16478fc01f51c8f9780210b
Kao Felix <kcfelix@gmail.com>
2019-04-30 09:56:41 +0200

    Use org-table face for markdown tables

--- custom.el
+++ custom.el
@@ -194,4 +194,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(markdown-table-face ((t (:inherit org-table)))))
