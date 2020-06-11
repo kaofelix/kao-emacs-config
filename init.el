1bc78be14feaa5237d1fb514b47f959f3f51071e
Kao Felix <kcfelix@gmail.com>
2020-06-11 08:54:24 +0200

    fix: update url for melpa

--- init.el
+++ init.el
@@ -17,7 +17,7 @@
 (setq user-mail-address "kcfelix@gmail.com")
 
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
 
