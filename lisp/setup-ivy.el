333b96db8fc2eb21b7f7fc6a7441ada8ff447c3a
Kao Felix <kcfelix@gmail.com>
2020-07-27 15:58:18 +0200

    feat: replace helm with ivy

---
+++ lisp/setup-ivy.el
@@ -0,0 +1,49 @@
+;;; setup-ivy.el --- Setup helm
+
+;; Copyright (C) 2020  Kao Felix
+
+;; Author: Kao Felix <kcfelix@gmail.com>
+;; Keywords: local
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+(use-package wgrep)
+(use-package flx)
+
+(use-package counsel
+  :after ivy
+  :delight
+  :config
+  (setq ivy-initial-inputs-alist nil)
+  (counsel-mode))
+
+(use-package counsel-projectile
+  :after counsel
+  :bind (:map projectile-command-map
+         ("s" . #'counsel-projectile-rg))
+  :config (counsel-projectile-mode))
+
+(use-package ivy
+  :defer 0.1
+  :delight
+  :bind (("C-c C-r" . ivy-resume))
+  :custom
+  (ivy-count-format "(%d/%d) ")
+  (ivy-use-virtual-buffers t)
+  (ivy-height 20)
+  :config
+  (setq ivy-re-builders-alist
+        '((swiper . ivy--regex-plus)
+          (counsel-rg . ivy--regex-plus)
+          (t . ivy--regex-fuzzy)))
+  (ivy-mode))
+
+(use-package swiper
+  :after ivy
+  :bind (("M-i" . swiper)))
+
+(provide 'setup-ivy)
+;;; setup-ivy.el ends here
--- lisp/setup-lsp.el
+++ lisp/setup-lsp.el
@@ -12,7 +12,6 @@
 
 (use-package lsp-ui :commands lsp-ui-mode)
 (use-package company-lsp :commands company-lsp)
-(use-package helm-lsp :commands helm-lsp-workspace-symbol)
 (use-package lsp-treemacs :commands lsp-treemacs-errors-list)
 
 ;; optionally if you want to use debugger
