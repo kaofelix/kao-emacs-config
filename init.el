7a5b1526e51de64c16808529c6180a7a5643f1dd
Kao Félix <kcfelix@gmail.com>
2021-01-04 18:35:59 +0100

    fix: set straight-vc-git-default-protocol to ssh

--- init.el
+++ init.el
@@ -21,6 +21,7 @@
 
 ;; straight.el + use-package setup
 (set-default 'straight-use-package-by-default t)
+(set-default 'straight-vc-git-default-protocol 'ssh)
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
