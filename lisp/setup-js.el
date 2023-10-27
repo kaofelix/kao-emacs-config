46ebcc9be1bb5d67c8927a5c1ca7d5676a6516ed
Kao Félix <kcfelix@gmail.com>
2023-10-28 00:21:49 +0200

    Simplify js/ts setup

--- lisp/setup-js.el
+++ lisp/setup-js.el
@@ -13,37 +13,10 @@
 (require 'use-package)
 
 (use-package add-node-modules-path
-  :after (typescript-mode)
-  :hook (typescript-mode . add-node-modules-path))
-
-(use-package typescript-mode
-  :after tree-sitter
-  :config
-  ;; we choose this instead of tsx-mode so that eglot can automatically figure out language for server
-  ;; see https://github.com/joaotavora/eglot/issues/624 and https://github.com/joaotavora/eglot#handling-quirky-servers
-  (define-derived-mode typescriptreact-mode typescript-mode
-    "TypeScript TSX")
-
-  ;; use our derived mode for tsx files
-  (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . typescriptreact-mode))
-  ;; by default, typescript-mode is mapped to the treesitter typescript parser
-  ;; use our derived mode to map both .tsx AND .ts -> typescriptreact-mode -> treesitter tsx
-  (add-to-list 'tree-sitter-major-mode-language-alist '(typescriptreact-mode . tsx)))
-
-;; https://github.com/orzechowskid/tsi.el/
-;; great tree-sitter-based indentation for typescript/tsx, css, json
-(use-package tsi
-  :after tree-sitter
-  :straight (:host github :repo "orzechowskid/tsi.el")
-  ;; define autoload definitions which when actually invoked will cause package to be loaded
-  :commands (tsi-typescript-mode tsi-json-mode tsi-css-mode)
-  :init
-  (add-hook 'typescript-mode-hook (lambda () (tsi-typescript-mode 1)))
-  (add-hook 'json-mode-hook (lambda () (tsi-json-mode 1)))
-  (add-hook 'css-mode-hook (lambda () (tsi-css-mode 1)))
-  (add-hook 'scss-mode-hook (lambda () (tsi-scss-mode 1)))
-  :config
-  (add-to-list 'dtrt-indent-hook-mapping-list '(typescript-mode default tsi-typescript-indent-offset)))
+  :after (typescript-ts-mode tsx-ts-mode)
+  :hook
+  (typescript-ts-mode . add-node-modules-path)
+  (tsx-ts-mode . add-node-modules-path))
 
 (provide 'setup-js)
 ;;; setup-js.el ends here
