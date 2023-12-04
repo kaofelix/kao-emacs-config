4f10a609a965a845ee7f80ef86acc6f7edac68e0
Kao Félix <kcfelix@gmail.com>
2023-12-04 13:45:37 +0100

    Updates to TS/JS modes

--- lisp/setup-js.el
+++ lisp/setup-js.el
@@ -12,11 +12,24 @@
 ;;; Code:
 (require 'use-package)
 
-(use-package add-node-modules-path
-  :after (typescript-ts-mode tsx-ts-mode)
+(defun kao/ts-eglot-organize-imports ()
+  (interactive)
+  (eglot-code-actions nil nil "source.organizeImports.ts" t)
+  (eglot-code-actions nil nil "source.removeUnusedImports.ts" t))
+
+(defun setup-typescript()
+  (eglot-ensure)
+  (eglot--code-action eglot-code-action-organize-imports "source.organizeImports.ts")
+  (eglot--code-action eglot-code-action-remove-unused-import "source.removeUnusedImports.ts")
+  (add-hook 'before-save-hook 'kao/ts-eglot-organize-imports nil t))
+
+(use-package typescript-ts-mode
   :hook
-  (typescript-ts-mode . add-node-modules-path)
-  (tsx-ts-mode . add-node-modules-path))
+  (typescript-ts-mode . setup-typescript)
+  (tsx-ts-mode . setup-typescript)
+  :init
+  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . tsx-ts-mode))
+  (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-ts-mode)))
 
 (provide 'setup-js)
 ;;; setup-js.el ends here
