c9c7ba0d436588182bb1e2f7f087607a80de96e5
Kao Félix <kcfelix@gmail.com>
2024-02-22 11:52:24 +0100

    Remove poetry mode and add pip-requirements editing support

--- lisp/setup-python.el
+++ lisp/setup-python.el
@@ -17,17 +17,14 @@
   (python-mode . eglot-ensure)
   (python-ts-mode . eglot-ensure))
 
-(use-package poetry
-  :hook
-  (python-mode . poetry-tracking-mode)
-  (python-ts-mode . poetry-tracking-mode))
-
 (use-package pyvenv
   :config
   (pyvenv-mode 1))
 
 (setq python-shell-interpreter "ipython"
       python-shell-interpreter-args "-i --simple-prompt")
+
+(use-package pip-requirements)
 
 (provide 'setup-python)
 ;;; setup-python.el ends here
