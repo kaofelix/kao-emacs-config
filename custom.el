ed419272b75b04930223997ffa16b751148d1e84
Kao Félix <kcfelix@gmail.com>
2024-09-29 22:32:30 +0200

    Delete broken custom-set-faces block

--- custom.el
+++ custom.el
@@ -95,8 +95,3 @@
  '(whitespace-global-modes nil)
  '(whitespace-style '(face trailing tabs))
  '(whitespace-trailing 'whitespace-space-before-tab t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
