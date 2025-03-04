3ae0c0fc9e20dab27b23818fb366115ea3504c5c
Kao Félix <kcfelix@gmail.com>
2025-03-04 22:57:57 +0100

    Cleanup old unused variables from custom.el

--- custom.el
+++ custom.el
@@ -4,14 +4,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ahs-face-check-include-overlay t)
- '(ahs-idle-interval 0.5)
- '(ahs-inhibit-face-list
-   '(font-lock-comment-delimiter-face font-lock-comment-face
-                                      font-lock-doc-face
-                                      font-lock-doc-string-face
-                                      font-lock-string-face
-                                      font-lock-keyword-face))
  '(ansi-color-names-vector
    ["#3c3836" "#fb4933" "#b8bb26" "#fabd2f" "#83a598" "#d3869b" "#8ec07c"
     "#ebdbb2"])
@@ -22,16 +14,9 @@
  '(bookmark-fringe-mark nil)
  '(bookmark-save-flag 1)
  '(bookmark-set-fringe-mark nil)
- '(browse-kill-ring-display-duplicates nil)
- '(browse-kill-ring-highlight-current-entry t)
- '(browse-kill-ring-highlight-inserted-item 'pulse)
- '(coffee-tab-width 2)
  '(consult-ripgrep-args
    "rg --hidden -g \"!.git/*\" --null --line-buffered --color=never --max-columns=1000 --path-separator /   --smart-case --no-heading --with-filename --line-number --search-zip")
  '(copilot-indent-offset-warning-disable t)
- '(custom-search-field nil)
- '(dap-auto-configure-features '(controls tooltip))
- '(dap-internal-terminal 'dap-internal-terminal-vterm)
  '(dired-dwim-target t)
  '(dtrt-indent-max-merge-deviation 15.0)
  '(dumb-jump-force-searcher 'rg)
@@ -42,17 +27,12 @@
  '(exec-path-from-shell-shell-name "/bin/zsh")
  '(exec-path-from-shell-variables '("PATH" "MANPATH" "WORKON_HOME"))
  '(git-commit-post-finish-hook '(git-gutter:update-all-windows))
- '(global-rbenv-mode nil)
- '(gofmt-command "goimports")
  '(imenu-auto-rescan t)
  '(inhibit-startup-screen t)
  '(locate-command "mdfind")
  '(magit-save-repository-buffers 'dontask)
- '(markdown-asymmetric-header t)
  '(minibuffer-depth-indicate-mode t)
- '(mmm-parse-when-idle t)
  '(native-comp-async-report-warnings-errors 'silent)
- '(orderless-matching-styles '(orderless-regexp orderless-initialism orderless-flex))
  '(org-catch-invisible-edits 'error)
  '(org-fold-catch-invisible-edits 'error)
  '(org-hide-leading-stars t)
@@ -61,12 +41,6 @@
  '(org-special-ctrl-a/e t)
  '(org-special-ctrl-k t)
  '(org-src-fontify-natively t)
- '(pdf-view-midnight-colors '("#fdf4c1" . "#32302f"))
- '(pico8-create-images nil)
- '(pico8-documentation-file "~/.emacs.d/etc/pico8/pico8_manual.txt")
- '(pos-tip-border-width 3)
- '(pos-tip-internal-border-width 8)
- '(py-underscore-word-syntax-p nil)
  '(python-check-command "ruff check")
  '(python-flymake-command '("ruff check"))
  '(register-preview-delay 0)
