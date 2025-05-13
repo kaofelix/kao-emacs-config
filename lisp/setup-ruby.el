48b26419e89c3d4f38a1937121725645c7a351ec
Kao Félix <kcfelix@gmail.com>
2025-05-13 21:14:44 +0200

    configure ruby-ts-mode to use ruby-lsp

--- lisp/setup-ruby.el
+++ lisp/setup-ruby.el
@@ -11,8 +11,12 @@
 
 ;;; Code:
 
-(with-eval-after-load 'eglot
-  (add-to-list 'eglot-server-programs '((ruby-mode ruby-ts-mode) . ("mise" "x" "--" "ruby-lsp"))))
+(use-package ruby-ts-mode
+  :hook (ruby-ts-mode . eglot-ensure)
+  :config
+  (add-to-list 'major-mode-remap-alist '(ruby-mode . ruby-ts-mode))
+  (with-eval-after-load 'eglot
+    (add-to-list 'eglot-server-programs '((ruby-ts-mode) . ("ruby-lsp")))))
 
 (provide 'setup-ruby)
 ;;; setup-ruby.el ends here
